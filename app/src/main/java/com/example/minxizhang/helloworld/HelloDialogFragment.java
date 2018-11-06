7d9ac7776c65a564cbdaf601961399283db33777
Mingxi Zhang <minxizhang@tripadvisor.com>
2018-11-06 19:55:10 +0800

    modify dialog theme

--- app/src/main/java/com/example/minxizhang/helloworld/HelloDialogFragment.java
+++ app/src/main/java/com/example/minxizhang/helloworld/HelloDialogFragment.java
@@ -3,6 +3,7 @@ package com.example.minxizhang.helloworld;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
@@ -24,18 +25,29 @@ public class HelloDialogFragment extends DialogFragment {
         tripNameView.setText("北京之旅");
         tripNameView.setSelection(0);
 
-        AlertDialog alertDialog = new AlertDialog.Builder(requireActivity())
+        AlertDialog alertDialog = new AlertDialog.Builder(requireActivity(), R.style.EditDlgFrag_DlgStyle)
                 .setView(savedTripView)
                 .setTitle("收藏至行程")
                 .setPositiveButton("保存", null)
                 .setNegativeButton("取消", null)
                 .create();
 
-        Window window = alertDialog.getWindow();
-        if (window != null) {
-            window.setSoftInputMode(LayoutParams.SOFT_INPUT_STATE_VISIBLE);
-        }
+//        Window window = alertDialog.getWindow();
+//        if (window != null) {
+//            window.setSoftInputMode(LayoutParams.SOFT_INPUT_STATE_VISIBLE |
+//                    LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+//        }
 
         return alertDialog;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Window window = getDialog().getWindow();
+        if (window != null) {
+            window.setSoftInputMode(LayoutParams.SOFT_INPUT_STATE_VISIBLE |
+                    LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+        }
     }
 }
